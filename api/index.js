8f21bd4cb6ea382a109b18b3626fd23abfe446ef
Saicharan <assaicharan06@gmail.com>
2026-08-13 22:45:55 +0530

    Normalize Vercel Express app export

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 'use strict';
 
-const app = require('../server/server');
+const serverModule = require('../server/server');
+
+const app =
+  serverModule?.default ||
+  serverModule?.app ||
+  serverModule;
+
+if (typeof app !== 'function') {
+  console.error('[VERCEL] Invalid Express app export:', {
+    type: typeof app,
+    keys: Object.keys(serverModule || {}),
+    defaultType: typeof serverModule?.default,
+    appType: typeof serverModule?.app,
+  });
+
+  throw new Error('Express app could not be resolved from server/server.js');
+}
 
 module.exports = (req, res) => {
   return app(req, res);
