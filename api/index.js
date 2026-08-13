199c82f209b80475c624537e15529b828deb52cf
Saicharan <assaicharan06@gmail.com>
2026-08-13 22:48:54 +0530

    Refactor Express app setup to server/app.js for clean Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,23 +1,5 @@
 'use strict';
 
-const serverModule = require('../server/server');
+const app = require('../server/app');
 
-const app =
-  serverModule?.default ||
-  serverModule?.app ||
-  serverModule;
-
-if (typeof app !== 'function') {
-  console.error('[VERCEL] Invalid Express app export:', {
-    type: typeof app,
-    keys: Object.keys(serverModule || {}),
-    defaultType: typeof serverModule?.default,
-    appType: typeof serverModule?.app,
-  });
-
-  throw new Error('Express app could not be resolved from server/server.js');
-}
-
-module.exports = (req, res) => {
-  return app(req, res);
-};
+module.exports = app;
